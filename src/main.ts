0fae07e8e0a9cb4fba835fa2c5e9edbac8c3cbc0
蒋国威 <jiangguowei@jiangguoweideMacBook-Air.local>
2023-04-08 11:13:22 +0800

    pinia持久化

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,17 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import pinia from '@/stores/index'
 import App from './App.vue'
 import router from './router'
 import '@/router/permission.ts'
 import './index.css'
 import globalComponent from '@/components/index'
 
-
-
 const app = createApp(App)
 // 注册全局的组件 （对所有需要注册的组件进行遍历并注册）
 for (const componentItme in globalComponent) {
-    app.component(componentItme, globalComponent[componentItme])
+  app.component(componentItme, globalComponent[componentItme])
 }
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
